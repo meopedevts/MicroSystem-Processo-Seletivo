10af9a82c8ea60ef527c698ce9ad647ff2c5679f
MeopeCego <marcelo.meopecego@gmail.com>
2023-02-22 14:13:12 -0300

    Parte 1 do processo

--- controle-candidatos/src/io/github/marcelosm/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/io/github/marcelosm/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,51 @@
 package io.github.marcelosm.candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
-    
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            //System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga, salário solicitado de" + salarioPretendido);
+                candidatosSelecionados++;
+            } else {
+                System.out.println("O candidado " + candidato + " não foi selecionado para a vaga, salário solicitado de " + salarioPretendido);
+            }
+            
+            candidatoAtual++;
+        
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
 }
